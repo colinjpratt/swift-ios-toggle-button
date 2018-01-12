df1b17d6b6df931eca670a6eb18f98ef03e027d2
colinjpratt <colinp@mediarain.com>
2018-01-11 22:44:00 -0700

    Creating a pod

---
+++ TwoStateButton/TwoStateButton.h
@@ -0,0 +1,19 @@
+//
+//  TwoStateButton.h
+//  TwoStateButton
+//
+//  Created by Colin on 1/11/18.
+//  Copyright © 2018 colinpratt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwoStateButton.
+FOUNDATION_EXPORT double TwoStateButtonVersionNumber;
+
+//! Project version string for TwoStateButton.
+FOUNDATION_EXPORT const unsigned char TwoStateButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwoStateButton/PublicHeader.h>
+
+
